8a75b9d034ed043eb80b3ade6dc13209ad874f9f
stella <sftamesis13@gmail.com>
2023-04-19 14:26:42 -0700

    setup signup component to handle user input

--- client/src/Signup.js
+++ client/src/Signup.js
@@ -2,28 +2,48 @@ import React, { useState, useContext } from 'react'
 import { UserContext } from './context/AuthContext'
 
 const Signup = () => {
-  const [name, setName] = useState("")
+  const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
   const [passwordConfirmation, setPasswordConfirmation] = useState("")
   const [errorsList, setErrorsList] = useState([])
   const {signup} = useContext(UserContext)
 
 
-  const handleSubmit = (e) => {
+  const handleSubmit = (e) => { //here we want the user to be created
     e.preventDefault()
-
+    fetch('/signup', {
+      method: "POST",
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+        username: username,
+        password: password,
+        password_confirmation: passwordConfirmation
+      })
+    })
+    .then(response => response.json())
+    .then(user => {
+      if (!user.errors) {
+        signup(user)
+      } else {
+        setUsername('')
+        setPassword('')
+        setPasswordConfirmation('')
+        const errorLis = user.errors.map(e => <li>{e}</li>)
+        setErrorsList(errorLis)
+      }
+    })
   }
 
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <label> Name: </label>
+        <label> Username: </label>
         <input 
           type="text"
-          id="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
         />
         <br/>
         <label> Password: </label>
