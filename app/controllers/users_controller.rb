85f8158fd5535b53e65a33cfd14c94e24c2e63ad
stella <sftamesis13@gmail.com>
2023-04-19 14:40:16 -0700

    Add create user signup function method in create with validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,14 @@
 class UsersController < ApplicationController
     def create
         #when a user signs up 
+        user = User.create(user_params)
+        if user.valid?
+            session[:user_id] = user.id #this is the moment this user is logged in.
+            render json: user
+        else
+            render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+        end
+        
     end
 
     def show
@@ -12,5 +20,11 @@ class UsersController < ApplicationController
         else 
             render json: { error: "Not Authorized" }, status: :unauthorized
         end
+    end
+
+    private
+
+    def user_params
+        params.permit(username, password, password_confirmation)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
+    validates :username, :password, :password_confirmation, presence: true
 
     has_many :schedules
     has_many :activities, through: :schedules
