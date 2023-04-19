704ba25e004ea7ad5c8921530d31b8cf7a249c89
stella <sftamesis13@gmail.com>
2023-04-19 13:39:43 -0700

    Add small modifications to file

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,22 @@ import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import './App.css';
 import Home from './Home'
+import Signup from './Signup';
+import Login from './Login';
 import { UserProvider } from './context/AuthContext';
+import Navbar from './Navbar';
+
 
 
 function App() {
   return (
     <div className="App">
       <UserProvider>
+        <Navbar />
         <Routes>
           <Route exact path='/' element={<Home />} />
+          <Route exact path='/signup' element={<Signup />} />
+          <Route exact path='/login' element={<Login />} />
         </Routes>
       </UserProvider>
     </div>
--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -3,9 +3,22 @@ import React, { useState, useEffect } from 'react';
 const UserContext = React.createContext();
 
 function UserProvider({ children }) {
+    const [user, setUser] = useState({})
+
+    useEffect(() => {
+        fetch("/me")
+            .then(response => response.json())
+            .then(data => {
+                setUser(data)
+            })
+    }, [])
+
+    const login = () => {}
+    const logout = () => {}
+    const signup = () => {}
 
     return(
-        <UserContext.Provider value={{}}>
+        <UserContext.Provider value={{user, login, logout, signup}}>
             {children}
         </UserContext.Provider>
     );
