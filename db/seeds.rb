ba46ba4fc0cea72b4705750bf2985bad833613da
stella <sftamesis13@gmail.com>
2023-04-19 12:06:55 -0700

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# Create some users
+user1 = User.create(name: "John", email: "john@example.com", password: "password")
+user2 = User.create(name: "Joe", email: "joe@example.com", password: "password")
+user3 = User.create(name: "Shawn", email: "shawn@example.com", password: "password")
+
+
+# Create some activities
+activity1 = Activity.create(name: "Swimming", description: "Fun in the pool", age_range: 6, duration: 60)
+activity2 = Activity.create(name: "Cooking", description: "Learn to cook", age_range: 8, duration: 90)
+activity3 = Activity.create(name: "Dancing", description: "Get your groove on", age_range: 10, duration: 120)
+
+# Add activities to users
+user1.activities << activity1
+user1.activities << activity2
+user2.activities << activity2
+user3.activities << activity3
+
+# Add user to activities
+activity1.users << user1
+activity2.users << user1
+activity2.users << user2
+activity3.users << user3
+
+# Create some schedules
+schedule1 = Schedule.create(date: Date.today, time_of_day: "Morning", user_id: user1.id, activity_id: activity1.id)
+schedule2 = Schedule.create(date: Date.tomorrow, time_of_day: "Afternoon", user_id: user1.id, activity_id: activity2.id)
+schedule3 = Schedule.create(date: Date.today, time_of_day: "Evening", user_id: user2.id, activity_id: activity2.id)
+schedule4 = Schedule.create(date: Date.today, time_of_day: "Morning", user_id: user3.id, activity_id: activity3.id)
