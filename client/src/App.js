4a2a2cda3f098a8b751a455f34a4d9e4da950a63
stella <sftamesis13@gmail.com>
2023-04-19 12:14:01 -0700

    Set global state for UserProvider

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-import logo from './logo.svg';
 import './App.css';
 import Home from './Home'
+import { UserProvider } from './context/AuthContext';
+
 
 function App() {
   return (
     <div className="App">
+      <UserProvider>
       <Routes>
         <Route exact path='/' element={<Home />} />
       </Routes>
+      </UserProvider>
     </div>
   );
 }
