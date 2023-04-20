ea8a117b07bd4652cfeea6d5d04643b4f37c7286
stella <sftamesis13@gmail.com>
2023-04-20 10:20:14 -0700

    add validations

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,8 @@
 class Activity < ApplicationRecord
     has_many :schedules
     has_many :users, through: :schedules
+
+    validates :name, :description, :age_range, :duration, presence: true
+    validates :age_range, :duration, numericality: true
+
 end
--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,4 +1,6 @@
 class Schedule < ApplicationRecord
   belongs_to :user
   belongs_to :activity
+
+  validates :date, :time_of_day, presence: true
 end
