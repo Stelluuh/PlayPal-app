d1b17b4cd3dbcad873acb2b6151407dcff9f7ff7
stella <sftamesis13@gmail.com>
2023-04-21 12:57:31 -0700

    add full CRUD

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,19 +1,34 @@
 class ActivitiesController < ApplicationController
 
-    def index
+    def index #shows all activities
         render json: Activity.all
     end
 
-   def destroy
-        activity = find_activity
-        activity.destroy
-        head :no_content
+   def show #shows a single activity
+      activity = find_activity
+      render json: activity
+   end
+
+   def update #updates an activity
+      activity = find_activity
+      activity.update(activity_params)
+      render json: activity
+   end
+
+   def destroy #deletes an activity
+      activity = find_activity
+      activity.destroy
+      head :no_content
    end
 
    private
 
    def find_activity
         Activity.find(params[:id])
+   end
+
+   def activity_params
+      params.permit(:name, :description, :age_range, :duration)
    end
 
 end
