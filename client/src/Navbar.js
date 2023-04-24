da117e7015305de2a80baf8c22b3b04b76b60e96
stella <sftamesis13@gmail.com>
2023-04-24 09:54:50 -0700

    fix login, logout, and signup feature

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -1,32 +1,39 @@
 import React, { useContext } from 'react'
 import { UserContext } from './context/AuthContext'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
   const { user, logout } = useContext(UserContext)
+  const navigate = useNavigate()
 
   const logoutUser = () => {
     fetch('/logout')
     .then(() => {
       logout()
+      navigate('/')
     })
   }
 
   if (user) {
     return (
       <div>
-        <h2>Welcome Back {user.name}</h2>
+        <h2>Hello {user.name}</h2>
         <br />
-        <button onClick={logout}>Logout</button>
+        <button onClick={logoutUser}>Logout</button>
       </div>
   
     )
   } else {
     return (
       <div>
+        <br />
         <NavLink to='/login'>
           <button>Login</button>
         </NavLink>
+        <NavLink to='/signup'>
+          <button>Signup</button>
+        </NavLink>
+        <hr />
       </div>
     )
   }
--- client/src/context/AuthContext.js
+++ client/src/context/AuthContext.js
@@ -6,29 +6,36 @@ const UserContext = createContext();
 //this component wraps the rest of our application and provides the authentication context to all of its children. So we created this UserProvider that is wrapped around our application. It accepts a children prop wich will be the component that it wraps.
 const UserProvider = ({ children }) => {
     const [user, setUser] = useState(null)
-
+    const [loggedIn, setLoggedIn] = useState(false) //add loggedIn Flag
+    
+    const navigate = useNavigate();
+    
     useEffect(() => {
         fetch("/me")
             .then(response => response.json())
             .then(data => {
                 setUser(data)
+                data.error ? setLoggedIn(false) : setLoggedIn(true) //set loggedIn to true if there is no error
             })
     }, [])
 
     const login = () => {
         setUser(user)
+        setLoggedIn(true)
     }
 
     const logout = () => {
         setUser(null)
+        setLoggedIn(false)
     }
 
     const signup = (user) => {
         setUser(user)
+        setLoggedIn(true)
     }
 
     return( //The values in this UserContext.Provider is what we are passing down to other components so that they can use this. (they an use the user, login, logout, and signup)
-        <UserContext.Provider value={{user, login, logout, signup}}>
+        <UserContext.Provider value={{user, login, logout, signup, loggedIn}}>
             {children}
         </UserContext.Provider>
     );
